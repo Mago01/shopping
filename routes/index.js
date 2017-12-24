543a94d4214b52d3f24dbb2f7dd95ac2e742e59d
Marco Orozco-Wallraff <marco.ag.orozco@gmail.com>
2017-12-23 16:48:26 -0800

    user login

--- routes/index.js
+++ routes/index.js
@@ -29,4 +29,14 @@ router.get('/users/profile', function(req, res, next) {
 	res.render('users/profile');
 });
 
+router.get('/users/signin', function(req, res, next){
+	var messages = req.flash('error');
+	res.render('users/signin', {csrfToken: req.csrfToken(), messages: messages, hasErrors: messages.length > 0});
+})
+
+router.post('/users/signin', passport.authenticate('local.signin',{
+	successRedirect:'/users/profile',
+	failureRedirect: '/users/signin',
+	failureFlash:true
+}))
 module.exports = router;
